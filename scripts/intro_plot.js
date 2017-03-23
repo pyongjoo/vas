4779d0e3b246d2592428494f518cc83026c260e9
Yongjoo Park <pyongjoo@umich.edu>
2017-03-23 12:33:11 -0400

    Update intro_plot.js

--- scripts/intro_plot.js
+++ scripts/intro_plot.js
@@ -3,7 +3,7 @@ $(function () {
 
   generatePlotlyChart('intro_chart', 'open_vas_density_geo_1000.csv', 'OpenStreeMap in 1,000 points (VAS)');
 
-  generatePlotlyChart('intro_chart2', 'open_rs_density_geo_1000.csv', 'OpenStreeMap in 1,000 points (VAS)');
+  generatePlotlyChart('intro_chart2', 'open_rs_density_geo_1000.csv', 'OpenStreeMap in 1,000 points (Uniform Random Sample)');
 
 });
 
